223edbf2ec1cc69870ac403d36dbd1be95b2d5d2
Alan Verdugo <factorpolar@protonmail.com>
2022-03-04 20:19:34 -0600

    Encoding message

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ document.querySelector('form').addEventListener('submit', event => {
     event.preventDefault();
 
     // Get the input string
-    const input = document.querySelector('input');
-    console.log(input.value);
+    const input = document.querySelector('#message-input');
+
+    // Encode the input using base 64
+    const encoded = btoa(input.value);
+
+    // Get the base domain and URL where this is running right now
+    const baseUrl = `${window.location}`;
+
+    // Show the URL with the encoded string into the other input object
+    const outputBox = document.querySelector("#link-input");
+    outputBox.value = `${baseUrl}#${encoded}`;
+
+    // For the convenience of the user, select the output URL
+    outputBox.select();
 });
